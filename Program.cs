65d694a8298b224e1198578c550427c449f49f7f
Jeroen-dH <proskater7437@gmail.com>
2023-05-12 13:07:59 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        pokemon charmander = new pokemon("beef", "fire", 100);
+        Console.WriteLine(charmander.getName());
+        Console.WriteLine(charmander.getType());
+    }
+}
+
+class pokemon
+{
+    public String name;
+    public String type;
+    public int hp;
+
+    public pokemon(string name, string type, int hp)
+    {
+        this.name = name;
+        this.type = type;
+        this.hp = hp;
+    }
+    public string getName()
+    {
+        return name;
+    }
+    public string getType()
+    {
+        return type;
+    }
+
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+    public void setType(string type)
+    {
+        this.type = type;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# GameCatalog
+# Pokemon
  
